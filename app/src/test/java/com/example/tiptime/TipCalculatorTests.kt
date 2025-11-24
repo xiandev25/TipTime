1f4cddb0783a8aafe67a68da854e3c74730b9e59
Xian Dev <xiandev25@gmail.com>
2025-11-24 12:38:54 +0100

    Write a local test for the calculateTip() method

---
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -0,0 +1,17 @@
+package com.example.tiptime
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipCalculatorTests {
+
+    @Test
+    fun calculateTip_20PercentNoRoundUp() {
+        val amount = 10.0
+        val tipPercent = 20.0
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, roundUp = false)
+        assertEquals(expectedTip, actualTip)
+    }
+}
