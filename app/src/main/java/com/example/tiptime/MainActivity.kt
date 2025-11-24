28d99951842081ce1e39006c14972d26392d9bb6
Xian Dev <xiandev25@gmail.com>
2025-11-24 03:54:27 +0100

    Add the logic to round up the calculated tip

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -120,7 +120,7 @@ fun TipTimeLayout() {
 
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     val tipPercentage = tipInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount, tipPercentage)
+    val tip = calculateTip(amount, tipPercentage, roundUp)
 
     Column(
         modifier = Modifier
@@ -178,8 +178,10 @@ fun TipTimeLayout() {
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+    var tip = tipPercent / 100 * amount
+    if (roundUp)
+        tip = kotlin.math.ceil(tip)
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
